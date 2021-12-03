7ec3f4b3ca7c829de75aad3205cb91cf092a3b94
Pradeep Raje <raje.pradeep@gmail.com>
2021-12-03 21:13:10 +0530

    module done 031122021

--- cairn.py
+++ cairn.py
@@ -1,5 +1,7 @@
 import io
 from flashtext import KeywordProcessor
+import sqlite3 as sq
+
 keyword_processor = KeywordProcessor()
 for item in ['Cairn Energy','Cairn plc','arbitration','retrospective tax','retro tax']:
 	keyword_processor.add_keyword(item)
@@ -23,26 +25,49 @@ headline: //*[@id="ctl00_ContentPlaceHolder1_lblHeadline"]
 
 module get element by xpath:
 ============================
-
+#imports segment
 from lxml import etree
-import requests,
+import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from flashtext import KeywordProcessor
 
+#early definition
+keyword_processor = KeywordProcessor()
+for item in ['Cairn Energy','Cairn plc','arbitration','retrospective tax','retro tax']:
+	keyword_processor.add_keyword(item)
 
-#pass url
-webpage = requests.get(url)
-soup = BeautifulSoup(webpage.content, "html.parser")
-dom = etree.HTML(str(soup))
-text=dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblHeadline"]')[0].text
+def get_text(url,keyword_processor):
+    #pass url and defined keyword processor
+    webpage = requests.get(url)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    dom = etree.HTML(str(soup))
+    text=dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblHeadline"]')[0].text
+    lfound = list(set(keyword_processor.extract_keywords(data)))
+    return (lfound)
+    
+def do_db(lfound, cur):
+    # Writing to table
+    try:
+        datex=dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblNewsDate"]')[0].text
+        id_date=datetime.strftime(datetime.strptime(datex, '%d-%m-%Y'), '%Y-%m-%d'))
+        id_pub=dom.xpath(' //*[@id="ctl00_ContentPlaceHolder1_lblPublication"]')[0].text
+        id_author= dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblAuthor"]')[0].text
+        id_headline= dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblHeadline"]')[0].text
+        sql = """INSERT INTO cairndb(date, publication,author, headline, items) VALUES(?,?,?,?,?)"""
+        data = (id_date, id_pub,id_author,id_headline, lfound)
+        cur.execute(sql,data)
+        #con.commit()
+    else:
+        sql = """INSERT INTO cairndb(date, publication,author, headline, items) VALUES(?,?,?,?,?)"""
+        data = ('*****', '*****','*****','*****', '*****')
+        cur.execute(sql,data)
+        #con.commit()
 
-lfound = list(set(keyword_processor.extract_keywords(data)))
+    
 
-if (("Cairn Energy" or "Cairn plc" in lfound)) and (('arbitration' or 'retrospective tax' or 'retro tax')in lfound):
-    datex=dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblNewsDate"]')[0].text
-    id_date=datetime.strftime(datetime.strptime(datex, '%d-%m-%Y'), '%Y-%m-%d'))
-    id_pub=date: dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblNewsDate"]')[0].text
-    id_publication=dom.xpath(' //*[@id="ctl00_ContentPlaceHolder1_lblPublication"]')[0].text
-    id_author= dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblAuthor"]')[0].text
-    id_headline= dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_lblHeadline"]')[0].text
-
+with sq.connect("cairndb.db") as con:
+    cur = con.cursor()    
+    if (("Cairn Energy" or "Cairn plc" in lfound)) and (('arbitration' or 'retrospective tax' or 'retro tax')in lfound):  
+        do_db(lfound,cur)
+    
