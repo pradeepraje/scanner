3fa2503ef28ecb9d20cb4ccb220b8a9af778133a
Pradeep Raje <raje.pradeep@gmail.com>
2021-12-06 13:28:04 +0530

    not getting anywhere

--- test.py
+++ test.py
@@ -10,3 +10,9 @@ soup = BeautifulSoup(webpage.content, "html.parser")
 dom = etree.HTML(str(soup))
 text=(dom.xpath('//*[@id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel2_lblNews"]/div[3]/span')[0].text)
 print(text)
+
+url='https://mtrack.merryspiders.com/NewsDetailsPublished.aspx?NewsId=e3E6Ggaq2uAX9SosCfBB7Q==&MediaType='
+webpage=requests.get(url)
+soup=BeautifulSoup(webpage.content,'html.parser')
+dom=lxml.etree.HTML(str(soup))
+print(dom.xtree.get_element_by_id("ctl00_ContentPlaceHolder1_TabContainer1_TabPanel2_lblNews").text_content())
